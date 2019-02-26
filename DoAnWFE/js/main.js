908b011872958116529677928749afd688fe5339
Hà Lương Lê <hasproIT@gmail.com>
2019-02-26 22:36:34 +0700

    Nop bai tap do an cuoi khoa

---
+++ DoAnWFE/js/main.js
@@ -0,0 +1,59 @@
+// navbar
+$(window).scroll(function() {
+    if($(this).scrollTop()>5) {
+        $( ".navbar-me" ).addClass("fixed-me");
+    } else {
+        $( ".navbar-me" ).removeClass("fixed-me");
+    }
+});
+// navbar
+
+// counter
+$(document).ready(function() {
+            $('.counter').counterUp({
+                delay: 10,
+                time: 1000
+            });
+        });
+// counter
+
+// recent
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+// recent end
+
+// list
+
+// list end
+
+// what
+$(document).ready(function() {
+    $('#Carousel').carousel({
+        interval: 5000
+    })
+});
+// what end
